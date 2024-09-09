136c932b051fd6e2a606f325324a8a78359188ee
pedram <pedram5204@gmail.com>
2024-09-10 00:19:10 +0330

    Update activity main layout

--- android-app/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ android-app/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -5,20 +5,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.quizapp.databinding.ActivityMainBinding
 import com.example.quizapp.view.MathActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button:Button
+    lateinit var binding: ActivityMainBinding
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.goToMath)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        button.setOnClickListener {
-            val intent = Intent(this,MathActivity::class.java)
+        binding.mathCD.setOnClickListener{
+            val intent = Intent(this@MainActivity,MathActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
